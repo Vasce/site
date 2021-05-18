9558f7c46c16eed238b4420d46d81b9a80635668
bilev-like-me <bilevspb@gmail.com>
2021-05-18 12:09:24 +0300

    images

--- elbib/biblio/models.py
+++ elbib/biblio/models.py
@@ -7,7 +7,7 @@ class Content(models.Model):
     title = models.CharField("Название", max_length=100, null=False, blank=False)
     author = models.CharField("Автор", max_length=100, null=False, blank=False)
     book = models.FileField("Документы", upload_to="books/", null=True, blank=True)
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField(upload_to='biblio/static/uploads/')
     bo = models.TextField("Библиографическое описание", max_length=1000, null=False, blank=True)
 
 
