8fa053398257fc2a87df1358d527cb493560d869
bilev-like-me <bilevspb@gmail.com>
2021-05-20 17:24:44 +0300

    pictures

--- elbib/biblio/models.py
+++ elbib/biblio/models.py
@@ -15,7 +15,7 @@ class Content(models.Model):
     title = models.CharField("Название", max_length=100, null=False, blank=False)
     author = models.CharField("Автор", max_length=100, null=False, blank=False)
     book = models.FileField("Документы", upload_to="books/", null=True, blank=True)
-    image = models.ImageField(upload_to='biblio/static/uploads/', default='biblio/static/css/painbook.png')
+    image = models.ImageField(upload_to='uploads/', default='uploads/painbook.png')
     bo = models.TextField("Библиографическое описание", max_length=1000, null=False, blank=True)
     razdel = models.ManyToManyField(Category)
     def __str__(self):
